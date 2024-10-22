0032e0fb3e88358c023d6accae09e0d0f6ab2ed1
iyad-abd <iyadabd87@gmail.com>
2024-10-23 01:43:54 +0300

    add process.env to index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 3000
+const port = process.env.port || 3000
  const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
